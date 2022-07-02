f37f5070ff14a2b9732a5a5deaade1bb007a174c
Jude Ubuntu <root@JUDE.localdomain>
2022-07-02 20:04:54 +0100

    second [fix] for 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,9 +4,9 @@ def replace_in_list(my_list, idx, element):
         return(my_list)
 
     length = len(my_list)
-    
-    if idx > length:
+
+    if idx > (length - 1):
         return(my_list)
 
     my_list[idx] = element 
-    return my_list
+    return(my_list)
