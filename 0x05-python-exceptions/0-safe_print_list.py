0e619dd404ff38ffcf6920555de70be8e7acdcae
Jude Ubuntu <root@JUDE.localdomain>
2022-07-18 19:59:04 +0100

    second [fix] for 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list[:x]:
             print(i, end='')
             n+=1
-        print()
-        return(n)
+        print ()
+        return (n)
     except (IndexError):
         print('Index does not exist')
