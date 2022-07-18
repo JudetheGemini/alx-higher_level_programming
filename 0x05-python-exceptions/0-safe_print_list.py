d1292eab1d8b79bfdab0fdec10dc78dee9ba2bb7
Jude Ubuntu <root@JUDE.localdomain>
2022-07-18 17:05:42 +0100

    adding readme file and 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        n = 0
+        for i in my_list[:x]:
+            print(i, end='')
+            n+=1
+    except (IndexError):
+        print('Index does not exist')
+    finally:
+        print()
+        return(n)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Directory containing exercises on python error exceptions
