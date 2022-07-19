4c0600ee77a4f551943caf9bd31c902995bdb4f7
Jude Ubuntu <root@JUDE.localdomain>
2022-07-19 05:23:47 +0100

    [fix] for 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,7 +11,7 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         else:
             count += 1
-
-        print()
-        return (count)
+            
+    print()
+    return (count)
                   
