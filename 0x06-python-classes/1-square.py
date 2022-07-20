2e60ded171fe8c6087613643e8dbf19b9c03d543
Jude Ubuntu <root@JUDE.localdomain>
2022-07-20 05:58:57 +0100

    second fix for 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square():
-    ''' Square class with a private atribute
-    '''
     def __init__(self, size):
+        ''' Square class with a private attribute
+        Args:
+        size(int): Size of the square
+        '''
         self.__size = size
